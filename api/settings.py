1ef75a23392cd3d7521ae13feb912aa016e9d68d
Userma1 <49586013+Userma1@users.noreply.github.com>
2024-10-04 00:32:24 -0400

    updated based on bing

--- api/settings.py
+++ api/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- build_files.sh
+++ build_files.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-echo "BUILD START"
-
-# Ensure pip is installed
-python3 -m ensurepip --upgrade
-
-# Install PostgreSQL and dependencies
-sudo apt-get update
-sudo apt-get install -y postgresql postgresql-contrib libpq-dev
-
-# Create a virtual environment
-python3 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Upgrade pip to the latest version
-pip install --upgrade pip
-
-# Install dependencies, using psycopg2-binary
+# Install dependencies
 pip install -r requirements.txt
-pip install psycopg2-binary
 
 # Collect static files
-python3 manage.py collectstatic --noinput
-
-echo "BUILD END"
+python manage.py collectstatic --no-input
