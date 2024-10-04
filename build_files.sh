14f757a4e3d432d8e678939f76c7f03e2abe4d14
Userma1 <49586013+Userma1@users.noreply.github.com>
2024-10-04 00:21:12 -0400

    updating static files directory

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,10 @@ echo "BUILD START"
 # Ensure pip is installed
 python3 -m ensurepip --upgrade
 
+# Install PostgreSQL and dependencies
+sudo apt-get update
+sudo apt-get install -y postgresql postgresql-contrib libpq-dev
+
 # Create a virtual environment
 python3 -m venv venv
 
@@ -14,8 +18,9 @@ source venv/bin/activate
 # Upgrade pip to the latest version
 pip install --upgrade pip
 
-# Install dependencies
+# Install dependencies, using psycopg2-binary
 pip install -r requirements.txt
+pip install psycopg2-binary
 
 # Collect static files
 python3 manage.py collectstatic --noinput
